484953c3eb96587b0ec063d498e3bfebbe5c00c0
Jason Swett <jason@benfranklinlabs.com>
2016-06-10 18:41:54 -0400

    Add BookListComponent.

--- client/src/app/author-wizard.component.ts
+++ client/src/app/author-wizard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { BookListComponent } from './+book-list';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'author-wizard-app',
   templateUrl: 'author-wizard.component.html',
-  styleUrls: ['author-wizard.component.css']
+  styleUrls: ['author-wizard.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/book-list', component: BookListComponent}
+])
 export class AuthorWizardAppComponent {
   title = 'author-wizard works!';
 }
