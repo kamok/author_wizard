cc017b845307b101cf19f22773793427091a874d
Jason Swett <jason@benfranklinlabs.com>
2016-06-11 00:27:21 -0400

    Include book form in New Book page.

--- client/src/app/book-form/book-form.component.ts
+++ client/src/app/book-form/book-form.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-book-form',
   templateUrl: 'book-form.component.html',
-  styleUrls: ['book-form.component.css']
+  styleUrls: ['book-form.component.css'],
 })
 export class BookFormComponent implements OnInit {
 
