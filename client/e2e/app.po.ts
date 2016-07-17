5852d4b741d0f6f385cfae84115798ef56a5fe81
Jason Swett <jason@benfranklinlabs.com>
2016-07-17 09:03:55 -0400

    Remove client directory.

--- client/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class AuthorWizardPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('author-wizard-app h1')).getText();
-  }
-}
--- client/src/app/+new-book/index.ts
+++
@@ -1,1 +0,0 @@
-export { NewBookComponent } from './new-book.component';
